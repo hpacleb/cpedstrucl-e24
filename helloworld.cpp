08b031cf26b972791f8da6d0d97ed8c80e5ddb0a
PogiASF <hnpacleb@fit.edu.ph>
2016-12-10 10:36:55 +0800

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+int main ()
+{
+	std::cout << "H" << std::endl;
+	std::cout << "E" << std::endl;
+	std::cout << "L" << std::endl;
+	std::cout << "L" << std::endl;
+	std::cout << "O" << std::endl;
+	std::cout << " " << std::endl;
+	std::cout << "W" << std::endl;
+	std::cout << "O" << std::endl;
+	std::cout << "R" << std::endl;
+	std::cout << "L" << std::endl;
+	std::cout << "D" << std::endl;
+	std::cout << "!" << std::endl;
+	
+	
+	return 0;
+}
